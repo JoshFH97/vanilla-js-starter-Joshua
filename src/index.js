ee0572f90acca2f3f4f3035e691bad574aa9a2aa
JoshFH97 <joshelxd@gmail.com>
2024-06-03 10:47:48 -0600

    estilo basico

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // Inserte el código aquí
+let lista = document.getElementById('lista')
+let adds = document.getElementById('adds')
+let  contador = document.getElementById('contador')
+let agregar = document.getElementById('agregar')
+
+function annadirLista() {
+    
+}
 
